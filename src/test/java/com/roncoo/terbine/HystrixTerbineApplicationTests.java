9792632dde23fc8392fcc5ceee2bb04dda7b0107
misswmn <568890375@qq.com>
2020-05-13 16:23:28 +0800

    first commit

---
+++ src/test/java/com/roncoo/terbine/HystrixTerbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roncoo.terbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixTerbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
